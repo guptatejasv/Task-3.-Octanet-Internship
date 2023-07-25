fd125eb137666e34d8617bd69e30ff65ba32b7be
Tejasv Kumar <guptatejasv86086@gmail.com>
2023-07-25 11:03:18 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,83 @@
+var x = 0;
+function reveal()
+{
+    
+    if(x==0){
+    document.getElementById('a1').style.display = 'block';
+    document.getElementById('b1').innerHTML = 'x';
+    x= 1;
+    }
+    else{
+        document.getElementById('a1').style.display = 'none';
+    document.getElementById('b1').innerHTML = '+';
+    x = 0;
+    }
+}
+function reveal1()
+{
+    if(x==0){
+    document.getElementById('a2').style.display = 'block';
+    document.getElementById('b2').innerHTML = 'x';
+    x= 1;
+    }
+    else{
+        document.getElementById('a2').style.display = 'none';
+    document.getElementById('b2').innerHTML = '+';
+    x = 0;
+    }
+}
+function reveal2()
+{
+    if(x==0){
+    document.getElementById('a3').style.display = 'block';
+    document.getElementById('b3').innerHTML = 'x';
+    x= 1;
+
+    }
+    else{
+        document.getElementById('a3').style.display = 'none';
+    document.getElementById('b3').innerHTML = '+';
+    x = 0;
+    }
+
+}
+function reveal3()
+{
+    if(x==0){
+    document.getElementById('a4').style.display = 'block';
+    document.getElementById('b4').innerHTML = 'x';
+    x= 1;
+    }
+    else{
+        document.getElementById('a4').style.display = 'none';
+    document.getElementById('b4').innerHTML = '+';
+    x = 0;
+    }
+
+}
+function reveal4()
+{
+    if(x==0){
+    document.getElementById('a5').style.display = 'block';
+    document.getElementById('b5').innerHTML = 'x';
+    x= 1;
+}
+else{
+        document.getElementById('a5').style.display = 'none';
+    document.getElementById('b5').innerHTML = '+';
+    x = 0;
+    }
+}
+function reveal5()
+{
+    if(x==0){
+    document.getElementById('a6').style.display = 'block';
+    document.getElementById('b6').innerHTML = 'x';
+    x= 1;
+}
+ else{
+        document.getElementById('a6').style.display = 'none';
+    document.getElementById('b6').innerHTML = '+';
+    x = 0;
+    }
+}
